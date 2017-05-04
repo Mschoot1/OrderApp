defbf87d9cdfb0427ca2cf196405ab8ce6934141
marnix van der schoot <marnix van der schoot>
2017-05-04 11:23:28 +0200

    commit directory adjustments

--- app/src/main/java/com/example/marni/orderapp/OrderApp/LogInActivity.java
+++ app/src/main/java/com/example/marni/orderapp/OrderApp/LogInActivity.java
@@ -1,7 +1,9 @@
-package com.example.marni.orderapp.Presentation;
+package com.example.marni.orderapp.OrderApp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.marni.orderapp.R;
 
@@ -11,5 +13,16 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 }
