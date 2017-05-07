adca6e7514d4235feb7bf158508e2fd3b15dd033
marnix van der schoot <marnix van der schoot>
2017-05-07 11:14:33 +0200

    drawer and toolbar added by Marnix

--- app/src/main/java/com/example/marni/orderapp/Presentation/Activities/AllergiesActivity.java
+++ app/src/main/java/com/example/marni/orderapp/Presentation/Activities/AllergiesActivity.java
@@ -1,7 +1,17 @@
 package com.example.marni.orderapp.Presentation.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
@@ -16,7 +26,9 @@ import java.util.ArrayList;
  * Created by Wallaard on 4-5-2017.
  */
 
-public class AllergiesActivity extends AppCompatActivity {
+public class AllergiesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private final String TAG = getClass().getSimpleName();
 
     private AllergiesGenerator allergiesGenerator = new AllergiesGenerator();
 
@@ -24,11 +36,95 @@ public class AllergiesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_allergies);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ArrayList<Allergy> allergies = allergiesGenerator.getAllergies();
 
         ListView listViewAllergies = (ListView) findViewById(R.id.allergies_listview);
         BaseAdapter allergiesAdapter = new AllergiesListviewAdapter(this, getLayoutInflater(), allergies);
         listViewAllergies.setAdapter(allergiesAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+//            case R.id.action_settings:
+//                // User chose the "Settings" item, show the app settings UI...
+//                return true;
+//
+//            case R.id.action_favorite:
+//                // User chose the "Favorite" action, mark the current item
+//                // as a favorite...
+//                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Log.i(TAG, item.toString() + " clicked.");
+
+        int id = item.getItemId();
+
+        Intent intent;
+
+        switch (id) {
+            case R.id.nav_assortment:
+                intent = new Intent(getApplicationContext(), ProductsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_my_order:
+//                intent = new Intent(getApplicationContext(), OrderActivity.class);
+//                startActivity(intent);
+                break;
+            case R.id.nav_order_history:
+                intent = new Intent(getApplicationContext(), OrderHistoryActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_top_up:
+//                intent = new Intent(getApplicationContext(), TopUpActivity.class);
+//                startActivity(intent);
+                break;
+            case R.id.nav_allergy_information:
+                intent = new Intent(getApplicationContext(), AllergiesActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/example/marni/orderapp/Presentation/Activities/OrderHistoryActivity.java
+++ app/src/main/java/com/example/marni/orderapp/Presentation/Activities/OrderHistoryActivity.java
@@ -1,15 +1,111 @@
 package com.example.marni.orderapp.Presentation.Activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.marni.orderapp.R;
 
-public class OrderHistoryActivity extends AppCompatActivity {
+public class OrderHistoryActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private final String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_history);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+//            case R.id.action_settings:
+//                // User chose the "Settings" item, show the app settings UI...
+//                return true;
+//
+//            case R.id.action_favorite:
+//                // User chose the "Favorite" action, mark the current item
+//                // as a favorite...
+//                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Log.i(TAG, item.toString() + " clicked.");
+
+        int id = item.getItemId();
+
+        Intent intent;
+
+        switch (id) {
+            case R.id.nav_assortment:
+                intent = new Intent(getApplicationContext(), ProductsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_my_order:
+//                intent = new Intent(getApplicationContext(), OrderActivity.class);
+//                startActivity(intent);
+                break;
+            case R.id.nav_order_history:
+                intent = new Intent(getApplicationContext(), OrderHistoryActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_top_up:
+//                intent = new Intent(getApplicationContext(), TopUpActivity.class);
+//                startActivity(intent);
+                break;
+            case R.id.nav_allergy_information:
+                intent = new Intent(getApplicationContext(), AllergiesActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
