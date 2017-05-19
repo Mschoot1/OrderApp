c9fb4b11bd3585ab4b6810c9c476365a5ae16229
Marc den Uil <mcuil@avans.nl>
2017-05-19 11:58:28 +0200

    Added Remember email functionality & changed login layout

--- app/src/main/java/com/example/marni/orderapp/Presentation/Activities/LogInActivity.java
+++ app/src/main/java/com/example/marni/orderapp/Presentation/Activities/LogInActivity.java
@@ -1,6 +1,8 @@
 package com.example.marni.orderapp.Presentation.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,16 +22,35 @@ public class LogInActivity extends AppCompatActivity implements
         LoginTask.SuccessListener {
 
     private EditText editTextEmail,editTextPassword;
+    private CheckBox checkBox;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    private static final String PREF_NAME = "prefs";
+    private static final String KEY_REMEMBER = "remember";
+    private static final String KEY_EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
         Button buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
 
+        checkBox = (CheckBox)findViewById(R.id.checkBox_login);
         editTextEmail = (EditText) findViewById(R.id.login_editTextEmailaddress);
         editTextPassword = (EditText) findViewById(R.id.login_editTextPassword);
+
+        if(sharedPreferences.getBoolean(KEY_REMEMBER, true)){
+            checkBox.setChecked(true);
+            editTextEmail.setText(sharedPreferences.getString(KEY_EMAIL, ""));
+        } else {
+            checkBox.setChecked(false);
+        }
 
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
 
@@ -36,9 +58,9 @@ public class LogInActivity extends AppCompatActivity implements
             public void onClick(View v) {
 
                 if (isValidEmail(editTextEmail.getText().toString())) {
+                    managePreferences();
                     login("https://mysql-test-p4.herokuapp.com/login");
                 } else {
-
                     Toast.makeText(LogInActivity.this, "Invalid email address", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -69,7 +91,7 @@ public class LogInActivity extends AppCompatActivity implements
     @Override
     public void successful(Boolean successful) {
         if (successful) {
-            Toast.makeText(this, "Logged in", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(getApplicationContext(), OrderHistoryActivity.class);
 
             startActivity(intent);
@@ -80,5 +102,17 @@ public class LogInActivity extends AppCompatActivity implements
 
     public static boolean isValidEmail(CharSequence target) {
         return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    private void managePreferences(){
+        if(checkBox.isChecked()){
+            editor.putString(KEY_EMAIL, editTextEmail.getText().toString().trim());
+            editor.putBoolean(KEY_REMEMBER, true);
+            editor.apply();
+        } else {
+            editor.remove(KEY_EMAIL);
+            editor.putBoolean(KEY_REMEMBER, false);
+            editor.apply();
+        }
     }
 }
