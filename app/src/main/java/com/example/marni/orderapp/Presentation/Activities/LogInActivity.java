803591d27892b5e786a35bf928430c437843b5da
Marc den Uil <mcuil@avans.nl>
2017-05-20 10:27:44 +0200

    Changed loginActivity (focus on passwordfield if email is being remembered)

--- app/src/main/java/com/example/marni/orderapp/Presentation/Activities/LogInActivity.java
+++ app/src/main/java/com/example/marni/orderapp/Presentation/Activities/LogInActivity.java
@@ -47,6 +47,7 @@ public class LogInActivity extends AppCompatActivity implements
 
         if(sharedPreferences.getBoolean(KEY_REMEMBER, true)){
             checkBox.setChecked(true);
+            editTextEmail.setFocusable(false);
             editTextEmail.setText(sharedPreferences.getString(KEY_EMAIL, ""));
         } else {
             checkBox.setChecked(false);
