48f4b4d58148bbb8f7eac594972f191904378238
Marc den Uil <marcuil75@gmail.com>
2017-05-23 12:42:31 +0200

    Changed bussines logic drawer

--- app/src/main/java/com/example/marni/orderapp/BusinessLogic/DrawerMenu.java
+++ app/src/main/java/com/example/marni/orderapp/BusinessLogic/DrawerMenu.java
@@ -45,36 +45,44 @@ public class DrawerMenu implements OrdersGetTask.OnOrderAvailable {
         switch (id) {
             case R.id.nav_assortment:
                 intent = new Intent(context, ProductsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.putExtra(JWT_STR, jwt);
                 intent.putExtra(USER, user);
                 context.startActivity(intent);
                 break;
             case R.id.nav_my_order:
                 intent = new Intent(context, MyOrderActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.putExtra(JWT_STR, jwt);
                 intent.putExtra(USER, user);
                 getCurrent("https://mysql-test-p4.herokuapp.com/order/current/" + user);
                 break;
             case R.id.nav_order_history:
                 intent = new Intent(context, OrderHistoryActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.putExtra(JWT_STR, jwt);
                 intent.putExtra(USER, user);
                 context.startActivity(intent);
                 break;
             case R.id.nav_top_up:
                 intent = new Intent(context, TopUpActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.putExtra(JWT_STR, jwt);
                 intent.putExtra(USER, user);
                 context.startActivity(intent);
                 break;
             case R.id.nav_allergy_information:
                 intent = new Intent(context, AllergiesActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.putExtra(JWT_STR, jwt);
                 intent.putExtra(USER, user);
                 context.startActivity(intent);
                 break;
             case R.id.nav_logout:
                 intent = new Intent(context, LogInActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                        Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(intent);
                 break;
         }
