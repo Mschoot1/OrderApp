1cc8acee973bece40488291f8e1c633a49d0dee0
marnix van der schoot <marnix van der schoot>
2017-05-28 13:44:31 +0200

    loginactivity fixed and launcher icon added

--- app/src/main/java/com/example/marni/orderapp/Presentation/Activities/LogInActivity.java
+++ app/src/main/java/com/example/marni/orderapp/Presentation/Activities/LogInActivity.java
@@ -1,6 +1,8 @@
-package com.example.marni.registerapp.Presentation.Presentation.Activities;
+package com.example.marni.orderapp.Presentation.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -9,58 +11,88 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.marni.registerapp.Presentation.AsyncKlassen.LoginTask;
-import com.example.marni.registerapp.R;
+import com.auth0.android.jwt.Claim;
+import com.auth0.android.jwt.JWT;
+import com.example.marni.orderapp.DataAccess.AccountAccess.LoginTask;
+import com.example.marni.orderapp.DataAccess.DeviceInfo.DevicePutTask;
+import com.example.marni.orderapp.R;
 
-public class LogInActivity extends AppCompatActivity implements LoginTask.SuccessListener {
-    private EditText editTextEmail,editTextPassword;
+import static com.example.marni.orderapp.DataAccess.AccountAccess.LoginTask.UNAUTHORIZED;
+
+public class LogInActivity extends AppCompatActivity implements
+        LoginTask.SuccessListener, DevicePutTask.SuccessListener {
+
+    public static final String JWT_STR = "jwt_str";
+    public static final String USER = "user";
+
+    private final String TAG = getClass().getSimpleName();
+
+    private EditText editTextEmail, editTextPassword;
+    private CheckBox checkBox;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    private static final String PREF_NAME = "prefs";
+    private static final String KEY_REMEMBER = "remember";
+    private static final String KEY_EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        Button button = (Button) findViewById(R.id.buttonSignIn);
-        editTextEmail = (EditText) findViewById(R.id.editTextEmailaddress);
-        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
-            public void onClick(View V) {
-                //EditText ed = (EditText) findViewById(R.id.editTextEmailaddress);
-                //EditText ed2 = (EditText) findViewById(R.id.editTextPassword);
+        Button buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
 
-                if(isValidEmail(editTextEmail.getText().toString()))
-                {
-                    login("https://mysql-test-p4.herokuapp.com/loginRegister");
+        checkBox = (CheckBox) findViewById(R.id.checkBox_login);
+        editTextEmail = (EditText) findViewById(R.id.login_editTextEmailaddress);
+        editTextPassword = (EditText) findViewById(R.id.login_editTextPassword);
+
+        if (sharedPreferences.getBoolean(KEY_REMEMBER, true)) {
+            checkBox.setChecked(true);
+            editTextEmail.setText(sharedPreferences.getString(KEY_EMAIL, ""));
+            editTextEmail.setSelection(editTextEmail.getText().length());
+        } else {
+            checkBox.setChecked(false);
+        }
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (isValidEmail(editTextEmail.getText().toString())) {
+                    login("https://mysql-test-p4.herokuapp.com/loginAuth");
+                    managePreferences();
                 } else {
-                    Toast.makeText(LogInActivity.this,"Invalid email adress",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LogInActivity.this, "Invalid email address", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
 
-                //if(ed2_text.isEmpty() || ed2_text.length() == 0 || ed2_text.equals("") || ed2_text == null)
-                if(isValidEmail(editTextPassword.getText().toString()))
-                {
-                    editTextPassword.setError("Enter password");
-                }
+        TextView textViewNoAccountYet = (TextView) findViewById(R.id.textViewNoAccountYet);
+
+        textViewNoAccountYet.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+
+                startActivity(intent);
             }
         });
     }
 
-    public void successful(Boolean successful) {
-        if (successful) {
-            Toast.makeText(this, "Logged in", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(getApplicationContext(), OrderHistoryActivity.class);
-
-            startActivity(intent);
-        } else {
-            Toast.makeText(this, "Login failed, please try again.", Toast.LENGTH_LONG).show();
-        }
-    }
-
-
+    @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
     void login(String ApiUrl) {
 
         LoginTask task = new LoginTask(this);
@@ -70,7 +102,45 @@ public class LogInActivity extends AppCompatActivity implements LoginTask.Succes
         task.execute(urls);
     }
 
+    @Override
+    public void successful(String response) {
+
+        Log.i(TAG, "response: " + response);
+
+        if (response.equals(UNAUTHORIZED)) {
+            Toast.makeText(this, "Login failed, please try again.", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Logged in", Toast.LENGTH_LONG).show();
+
+            JWT jwt = new JWT(response);
+            Claim user = jwt.getClaim("user");
+            Intent intent = new Intent(getApplicationContext(), OrderHistoryActivity.class);
+            intent.putExtra(JWT_STR, jwt);
+            intent.putExtra(USER, user.asInt());
+
+            Log.i(TAG, "user.asInt(): " + user.asInt());
+            startActivity(intent);
+        }
+    }
+
     public static boolean isValidEmail(CharSequence target) {
         return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    private void managePreferences() {
+        if (checkBox.isChecked()) {
+            editor.putString(KEY_EMAIL, editTextEmail.getText().toString().trim());
+            editor.putBoolean(KEY_REMEMBER, true);
+            editor.apply();
+        } else {
+            editor.remove(KEY_EMAIL);
+            editor.putBoolean(KEY_REMEMBER, false);
+            editor.apply();
+        }
+    }
+
+    @Override
+    public void successfulPut(Boolean successful) {
+
     }
 }
